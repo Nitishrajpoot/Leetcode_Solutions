d4080fb4e01031b444182ef63d13da3a3613dcce
Nitishrajpoot <nitish.rajpoot212@gmail.com>
2026-08-09 10:10:46 +0530

    Time: 0 ms (100%), Space: 44.3 MB (44.91%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,28 +10,43 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        int size = 0;
-        ListNode node = head;
-        while(node != null){
-            size++;
-            node = node.next;
+
+        if(head == null || head.next == null || k==0)
+            return head;
+
+        int sz = 1;
+        ListNode temp = head;
+
+        // find length and tail
+        while(temp.next != null){
+            temp = temp.next;
+            sz++;
         }
-        k = k%size;
-        while(k-- > 0){
-            
-            fast = fast.next;
+
+        k = k % sz;
+
+        if(k==0)
+            return head;
+
+        // make circular
+        temp.next = head;
+
+
+        // find new tail
+        int steps = sz-k;
+
+        while(steps-- > 0){
+            temp = temp.next;
         }
-        
-        while(fast.next!=null){
-            slow = slow.next;
-            fast = fast.next;
-        }
-        fast.next = head;
-        node = slow.next;
-        slow.next = null;
-        return node;
+
+
+        // new head
+        ListNode newHead = temp.next;
+
+        // break cycle
+        temp.next = null;
+
+
+        return newHead;
     }
 }
